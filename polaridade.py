803a1fea696de314f3ee63fdbca3b9fe054bcb35
clarissacastella <clarissacastella@gmail.com>
2018-08-21 17:02:52 -0300

    working

--- polaridade.py
+++ polaridade.py
@@ -3,17 +3,18 @@ from nltk.classify import NaiveBayesClassifier, MaxentClassifier, SklearnClassif
 import csv
 from sklearn.svm import LinearSVC
 
-def word_split(data):    
+def divide(data):    
     data_new = []
     for word in data:
         word_filter = [i.lower() for i in word.split()]
         data_new.append(word_filter)
     return data_new
 
-def word_feats(words):    
+def featx(words):    
     return dict([(word, words.count(word)) for word in words])
-
-def evaluate_classifier(featx, sentences):
+    
+    
+def treina_classificadores():
         posdata = []
         with open('./dadostreino/train_EPTC_POA_v3nbal_1.data', 'rb') as myfile:    
             reader = csv.reader(myfile, delimiter=',')
@@ -29,9 +30,9 @@ def evaluate_classifier(featx, sentences):
             reader = csv.reader(myfile, delimiter=',')
             for val in reader:
                 neudata.append(val[0])                         
-        negfeats = [(featx(f), 'neg') for f in word_split(negdata)]
-        posfeats = [(featx(f), 'pos') for f in word_split(posdata)]
-        neufeats = [(featx(f), 'neu') for f in word_split(neudata)]
+        negfeats = [(featx(f), 'neg') for f in divide(negdata)]
+        posfeats = [(featx(f), 'pos') for f in divide(posdata)]
+        neufeats = [(featx(f), 'neu') for f in divide(neudata)]
 
         training_this_round = negfeats + posfeats+ neufeats
         #'Maximum Entropy'
@@ -41,17 +42,22 @@ def evaluate_classifier(featx, sentences):
         classifierSVM.train(training_this_round)
         # Naive Bayes
         classifierNB = NaiveBayesClassifier.train(training_this_round)
-			                        
+        return ([classifierME,classifierSVM,classifierNB])
+        
+def classifica(sentences, classificadores):                        
         for s in sentences:
-            c = word_split([s])
+            c = divide([s])
             feats= featx(c[0])
             observed = []
-            observed.append(classifierSVM.classify(feats))
-            observed.append(classifierNB.classify(feats))
-            observed.append(classifierME.classify(feats))
+            observed.append(classificadores[1].classify(feats))
+            observed.append(classificadores[2].classify(feats))
+            observed.append(classificadores[0].classify(feats))
             print s, observed
-                    	
+######## MAIN                    	
+classificadores = treina_classificadores()
+
 sentences = ['Queda de poste na Av. Sertório x R. Diamantina. Av Sertório sentido C/B totalmente bloqueada. Agentes da EPTC no local.', \
             'Não há mais manifestações na Est. Afonso Lourenço Mariante. Trânsito fluindo sem problemas.', \
             'Não use o celular ao volante, 80% da sua atenção é desviada.']
-evaluate_classifier(word_feats,sentences)
+            
+classifica(sentences, classificadores)
