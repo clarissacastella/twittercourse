f5b710ed7fa31bd25249d1c39443137ed5e4214a
clarissacastella <clarissa@meedan.com>
2018-10-13 19:25:50 -0300

    Upd

--- acessarAPI.py
+++ acessarAPI.py
@@ -1,6 +1,4 @@
-import urllib
 import oauth2 
-import json
 
 CHAVE_CONSUMO =	'42MJDzYEeD4563DT5sD7TDj1B'
 TOKEN_CONSUMO =	'h2n9lefM7T9qTKhf1Xjd4s9TuaecYHLDd9G0PYW0nZqhpKL4rp'
@@ -8,21 +6,9 @@ TOKEN_CONSUMO =	'h2n9lefM7T9qTKhf1Xjd4s9TuaecYHLDd9G0PYW0nZqhpKL4rp'
 TOKEN_ACESSO =	'3525322276-CzzzJWJQbi9aYbwEC2KWLaIZJWYbmPmwkrPlYIe'
 CHAVE_ACESSO =	'67d9rBkqTT261zjxFvuDnO0JnBxqBwq5svh6JnOreYkoO'
 
-
-
 def oauth_req(url, CHAVE_ACESSO, TOKEN_ACESSO, http_method="GET", post_body="", http_headers=None):
     token = oauth2.Token(key=CHAVE_ACESSO, secret=TOKEN_ACESSO)
     consumo = oauth2.Consumer(key=CHAVE_CONSUMO, secret=TOKEN_CONSUMO)
     cliente = oauth2.Client(consumo, token)
     resp, conteudo = cliente.request( url, method=http_method, body=post_body, headers=http_headers )
-
     return conteudo
-
-
-def info_usr(usuario):
-    GET_USR_URL = "https://api.twitter.com/1.1/users/show.json?screen_name="+usuario
-    req = oauth_req(GET_USR_URL,TOKEN_ACESSO,CHAVE_ACESSO)
-    return req
-
-d = json.loads( info_usr('twitterbrasil'))
-print json.dumps(d, indent=4, sort_keys=True)
